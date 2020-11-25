be15b29e0c47491ac2f45d797b44e6bd85ea8625
Woomy4680-exe <matzar1808@gmail.com>
2020-11-25 16:09:12 +0100

    💄 Added navbar

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<navbar></navbar>
+</template>
+
+<script>
+import navbar from "./components/Navbar"
+export default {
+  name: 'App',
+  components: {
+    navbar
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+* {
+  font-family: 'Roboto', sans-serif;
+}
+div, body, html{
+  padding: 0;
+  display: block;
+  margin-top: 0px;
+  overflow: hidden;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+        <ul class="navbar">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="https://github.com/DetectionHSL" target="_blank">GitHub</a></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style>
+.navbar {
+    background-color: #333;
+    overflow: hidden;
+    height: 100%;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+   
+   
+     
+    
+}
+a:hover {
+    color: lightgreen;
+}       
+a {
+    float: left;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;      
+}
+</style>
